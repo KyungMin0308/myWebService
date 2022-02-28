54d25ea675bf15e9b2b5d6280b44c3fe7d9e04a9
KyungMin0308 <km3462@naver.com>
2022-02-28 22:48:04 +0900

    스프링 시큐리티 & OAuth2.0 적용하기.

--- src/main/java/com/kyungmin/mywebservice/controller/IndexController.java
+++ src/main/java/com/kyungmin/mywebservice/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.kyungmin.mywebservice.controller;
 
+import com.kyungmin.mywebservice.config.auth.LoginUser;
+import com.kyungmin.mywebservice.config.auth.dto.SessionUser;
 import com.kyungmin.mywebservice.dto.PostsResponseDto;
 import com.kyungmin.mywebservice.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +18,13 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
--- src/test/java/com/kyungmin/mywebservice/controller/HelloControllerTest.java
+++ src/test/java/com/kyungmin/mywebservice/controller/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.kyungmin.mywebservice.controller;
 
+import com.kyungmin.mywebservice.config.auth.SecurityConfig;
 import com.kyungmin.mywebservice.controller.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -16,12 +20,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행, Spring Boot Test와 Junit 사이에 연결자
-@WebMvcTest(controllers = HelloController.class) //Web(Spring MVC)에 집중할 수 있는 Annotation(@Controller, @ControllerAdvice 사용 가능)
+//Web(Spring MVC)에 집중할 수 있는 Annotation(@Controller, @ControllerAdvice 사용 가능)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc; //웹 API를 테스트할 때 사용, Spring MVC 테스트의 시작점
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -31,6 +39,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); //mvc.perform의 결과 검증(응답 본문)
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
