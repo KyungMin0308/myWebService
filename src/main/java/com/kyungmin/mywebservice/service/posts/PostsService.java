6e2e0c8558e02f37961d5500d7a59f2ad126a981
KyungMin0308 <km3462@naver.com>
2022-02-28 15:03:33 +0900

    Mustache로 화면 구성하기.

--- src/main/java/com/kyungmin/mywebservice/service/posts/PostsService.java
+++ src/main/java/com/kyungmin/mywebservice/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.kyungmin.mywebservice.service.posts;
 
 import com.kyungmin.mywebservice.domain.posts.Posts;
 import com.kyungmin.mywebservice.domain.posts.PostsRepository;
+import com.kyungmin.mywebservice.dto.PostsListResponseDto;
 import com.kyungmin.mywebservice.dto.PostsResponseDto;
 import com.kyungmin.mywebservice.dto.PostsSaveRequestDto;
 import com.kyungmin.mywebservice.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
